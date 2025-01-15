ddb9d99c31579ee4a7092e04eaaedebb13fb8ad5
AI-bot-1232 <nries2026@kehillahstudent.org>
2025-01-15 12:08:31 -0800

    finished

--- main.py
+++ main.py
@@ -27,3 +27,21 @@ print(encrypt(password))
 # Your task:
 # figure out what key I used to encrypt this message:
 secret_message = "y qc q iushuj cuiiqwu oek mybb duluh wkuii"
+
+def decrypt(ciphertext, test_key):
+    for I in range(10):
+        plaintext = ""
+    for letter in ciphertext:
+        old_position = alphabet.find(letter)
+        if old_position == -1:
+            plaintext += ''
+        else:
+            new_position = old_position - test_key
+            new_position = new_position % len(alphabet)
+            new_letter = alphabet[new_position]
+            plaintext += new_letter
+    return plaintext
+
+for possible_key in range(17):
+    decrypted = decrypt(secret_message, possible_key)
+    print(f"Key {possible_key}: {decrypted}")
